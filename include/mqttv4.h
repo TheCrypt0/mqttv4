d597d664a720f269cf187635a75ca0bbd7a032fa
TheCrypt0 <cryptoofficial@gmail.com>
2019-04-18 18:44:44 +0200

    [Add] Added configuration parsing (UGLY and repetitive)

--- include/mqttv4.h
+++ include/mqttv4.h
@@ -9,10 +9,19 @@
 #include <string.h>
 #include <time.h>
 
+#include "config.h"
 #include "ipc.h"
 #include "mqtt.h"
 
 #define MQTTV4_VERSION      "0.0.1"
 #define MQTTV4_CONF_FILE    "/home/yi-hack-v4/etc/mqttv4.conf"
+
+typedef struct
+{
+    char *mqtt_prefix;
+    char *topic_motion;
+    char *motion_start_msg;
+    char *motion_stop_msg;
+} mqttv4_conf_t;
 
 #endif // MQTTV4_H
--- src/mqttv4.c
+++ src/mqttv4.c
@@ -1,20 +1,60 @@
 #include "mqttv4.h"
 
+/*
+ * Just a quick disclaimer.
+ * This code is ugly and it is the result of some testing
+ * and madness with the MQTT.
+ *
+ * It will be probably re-written from the ground up to
+ * handle more configurations, messages and callbacks.
+ *
+ * Crypto
+ */
+
+mqtt_conf_t conf;
+mqttv4_conf_t mqttv4_conf;
+
+static void init_mqttv4_config();
+static void handle_config(const char *key, const char *value);
+
 void callback_motion_start()
 {
+    char topic[128];
+    mqtt_msg_t msg;
+
     printf("CALLBACK MOTION START\n");
+
+    msg.msg=mqttv4_conf.motion_start_msg;
+    msg.len=strlen(msg.msg);
+    msg.topic=topic;
+
+    sprintf(topic, "%s/%s", mqttv4_conf.mqtt_prefix, mqttv4_conf.topic_motion);
+
+    mqtt_send_message(&msg);
 }
 
 void callback_motion_stop()
 {
+    char topic[128];
+    mqtt_msg_t msg;
+
     printf("CALLBACK MOTION STOP\n");
+
+    msg.msg=mqttv4_conf.motion_stop_msg;
+    msg.len=strlen(msg.msg);
+    msg.topic=topic;
+
+    sprintf(topic, "%s/%s", mqttv4_conf.mqtt_prefix, mqttv4_conf.topic_motion);
+
+    mqtt_send_message(&msg);
 }
 
 int main(int argc, char **argv)
 {
     int ret;
 
-    mqtt_conf_t conf;
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
 
     ret=init_mqtt();
     if(ret!=0)
@@ -22,7 +62,7 @@ int main(int argc, char **argv)
 
     mqtt_init_conf(&conf);
 
-    strcpy(conf.host, "server-test");
+    init_mqttv4_config();
 
     mqtt_set_conf(&conf);
 
@@ -39,22 +79,100 @@ int main(int argc, char **argv)
 
     while(1)
     {
-        mqtt_msg_t msg;
-        char test[]="TEST_123";
-        char topic[]="test/1";
-
-        msg.msg=test;
-        msg.len=strlen(test);
-        msg.topic=topic;
-
-        //printf("Sending message...\n");
-
-        mqtt_send_message(&msg);
-
         mqtt_loop();
-
         usleep(500*1000);
     }
 
     return 0;
+}
+
+static void handle_config(const char *key, const char *value)
+{
+    int nvalue;
+
+    // Ok, the configuration handling is UGLY, unsafe and repetitive.
+    // It should be fixed in the future by writing a better config
+    // handler or by just using a library.
+
+    // If you think to have a better implementation.. PRs are welcome!
+
+    if(strcmp(key, "MQTT_IP")==0)
+    {
+        strcpy(conf.host, value);
+    }
+    else if(strcmp(key, "MQTT_USER")==0)
+    {
+        conf.user=malloc((char)strlen(value)+1);
+        strcpy(conf.user, value);
+    }
+    else if(strcmp(key, "MQTT_PASSWORD")==0)
+    {
+        conf.password=malloc((char)strlen(value)+1);
+        strcpy(conf.password, value);
+    }
+    else if(strcmp(key, "MQTT_PORT")==0)
+    {
+        errno=0;
+        nvalue=strtol(value, NULL, 10);
+        if(errno==0)
+            conf.port=nvalue;
+    }
+    else if(strcmp(key, "MQTT_KEEPALIVE")==0)
+    {
+        errno=0;
+        nvalue=strtol(value, NULL, 10);
+        if(errno==0)
+            conf.keepalive=nvalue;
+    }
+    else if(strcmp(key, "MQTT_QOS")==0)
+    {
+        errno=0;
+        nvalue=strtol(value, NULL, 10);
+        if(errno==0)
+            conf.qos=nvalue;
+    }
+    else if(strcmp(key, "MQTT_RETAIN")==0)
+    {
+        errno=0;
+        nvalue=strtol(value, NULL, 10);
+        if(errno==0)
+            conf.retain=nvalue;
+    }
+    else if(strcmp(key, "MQTT_PREFIX")==0)
+    {
+        mqttv4_conf.mqtt_prefix=malloc((char)strlen(value)+1);
+        strcpy(mqttv4_conf.mqtt_prefix, value);
+    }
+    else if(strcmp(key, "TOPIC_MOTION")==0)
+    {
+        mqttv4_conf.topic_motion=malloc((char)strlen(value)+1);
+        strcpy(mqttv4_conf.topic_motion, value);
+    }
+    else if(strcmp(key, "MOTION_START_MSG")==0)
+    {
+        mqttv4_conf.motion_start_msg=malloc((char)strlen(value)+1);
+        strcpy(mqttv4_conf.motion_start_msg, value);
+    }
+    else if(strcmp(key, "MOTION_STOP_MSG")==0)
+    {
+        mqttv4_conf.motion_stop_msg=malloc((char)strlen(value)+1);
+        strcpy(mqttv4_conf.motion_stop_msg, value);
+    }
+    else
+    {
+        printf("key: %s | value: %s\n", key, value);
+        fprintf(stderr, "Unrecognized config.\n");
+    }
+}
+
+static void init_mqttv4_config()
+{
+    if(init_config(MQTTV4_CONF_FILE)!=0)
+    {
+        printf("Cannot open config file. Skipping.\n");
+        return;
+    }
+
+    config_set_handler(&handle_config);
+    config_parse();
 }
